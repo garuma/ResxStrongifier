c7d58372c98578188a08d9d6854bcdc50ad1f1ce
Alan McGovern <alan@xamarin.com>
2018-10-25 14:43:58 +0100

    Generate the .designer.cs file too
    This means when you reference the nuget the only thing you need to
    do is set the 'Generator' property of the resx file to be
    'MSBuild::GenerateStringResources'. After that the .Designer.cs and
    the .Strings.cs will be generated automatically.
    
    We additionally support the 'CustomToolNamespace' and 'Public' properties
    to control the namespace and public/internal of the generated class.
    
    A full file may look something like this:
    
    <EmbeddedResource Update="Files\Resource.resx">
      <CustomToolNamespace>My.Alternate.Namespace</CustomToolNamespace>
      <Generator>MSBuild::GenerateStringResources</Generator>
      <Public>true</Public>
      <TargetClassName>MyCustomClass</TargetClassName>
    </EmbeddedResource>
    
    This will generate Resource.Designer.cs and Resource.Strings.cs.
    
    The class in the '.Designer.cs' file will be called 'Resource' and
    in the '.Strings.cs' file it is overrridden to be 'MyCustomClass'

---
+++ GenerateCombinedResources.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace NetFx
+{
+	/// <summary>
+	/// Generates a typed class for the given input .resx files.
+	/// </summary>
+	public class GenerateCombinedResources : Task
+	{
+		/// <summary>
+		/// Default class name to use if no TargetClassName metadata is provided
+		/// for the input resx files.
+		/// </summary>
+		public const string DefaultClassName = "Strings";
+
+		/// <summary>
+		/// Language of the containing project.
+		/// </summary>
+		[Required]
+		public string Language { get; set; }
+
+		/// <summary>
+		/// The resource files to process.
+		/// </summary>
+		[Required]
+		public ITaskItem[] ResxFiles { get; set; }
+
+		/// <summary>
+		/// Root namespace for the containing project.
+		/// </summary>
+		[Required]
+		public string RootNamespace { get; set; }
+
+		/// <summary>
+		/// Generated stronly typed code files.
+		/// </summary>
+		[Output]
+		public ITaskItem[] GeneratedFiles { get; set; }
+
+		/// <summary>
+		/// Generates the strong typed resources for the given resx input files.
+		/// </summary>
+		/// <remarks>a remark</remarks>
+		public override bool Execute()
+		{
+			if (Language != "C#")
+			{
+				Log.LogError("Language {0} is not supported yet.", Language);
+				return !Log.HasLoggedErrors;
+			}
+
+			var generatedFiles = new List<ITaskItem>(ResxFiles.Length);
+
+			foreach (var resx in ResxFiles)
+			{
+				bool generateStringResources;
+				if (!bool.TryParse (resx.GetMetadata("GenerateStringResources"), out generateStringResources))
+					generateStringResources = true;
+
+				var stringResources = generateStringResources ? StringResources.Execute(Language, resx, RootNamespace, Log) : "";
+				var designerResources = DesignerResources.Execute(Language, resx, RootNamespace, Log);
+
+				var targetFile = resx.GetMetadata ("GeneratedOutput");
+				Directory.CreateDirectory (Path.GetDirectoryName (targetFile));
+				File.WriteAllText(targetFile, stringResources + System.Environment.NewLine + System.Environment.NewLine + designerResources);
+
+				generatedFiles.Add(new TaskItem(resx) {
+					ItemSpec = targetFile
+				});
+			}
+
+			GeneratedFiles = generatedFiles.ToArray();
+
+			return !Log.HasLoggedErrors;
+		}
+	}
+}
